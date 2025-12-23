d90c2ea0704a4140a91375807589fdd1dc74159b
GN68 <81744903+GN68@users.noreply.github.com>
2025-12-24 02:39:46 +0800

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
-pyinstaller \
+#!/usr/bin/env bash
+set -e
+
+
+rm -rf build dist *.egg-info
+
+.venv/bin/python -m PyInstaller \
 --onefile \
 --collect-submodules ftk.cli.commands \
 --name figura-toolkit \
-figura-toolkit/main.py
+ftk/__main__.py
+
+
+chmod +x dist/figura-toolkitecho $SHELL
